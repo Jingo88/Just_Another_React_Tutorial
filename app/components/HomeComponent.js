86ceed5220687e20959b369c1be30c57b07355ac
Jason <Jcory.ng@gmail.com>
2016-11-03 13:14:58 -0400

    some styling

--- app/components/HomeComponent.js
+++ app/components/HomeComponent.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+const styles = {
+	centering: {
+		textAlign: 'center'
+	}
+}
+
 function HomeComponent(props){
 	return(
-		<div>
-			<h1>Hello World!!!</h1>
-			<h3>You're using REACT!!!!</h3>
+		<div className="container" style={styles.centering}>
+			<h1>This Is Your Movie App!</h1>
+			<div className="row">
+		    <form className="col s10 offset-s2 m4 offset-m4"
+		    	onSubmit = {props.onSubmitMovie}>
+		    	<input 
+		    		placeholder="Enter Movie or TV Show Title" 
+		    		type="text"
+		    		className="validate"
+		    		onChange = {props.onUpdateMovie}/>
+		    	<input 
+		    		type="submit"
+		    		hidden/>
+		    </form>
+		  </div>
 		</div>
 	)
 }
